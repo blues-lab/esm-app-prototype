d451fa33d9a34fd231df82f22c8b1a21058833a9
Rakibul Hasan <rakib_cse_062@yahoo.com>
2019-06-28 11:02:36 -0700

    Third attempt to make wifi work

---
+++ App.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+//import * as RNFS from 'react-native-fs';
+import wifi from 'react-native-android-wifi';
+
+export default class App extends Component<Props> 
+{
+
+  state = {msg: "Not tried yet"};
+
+  UpdateWifiState()
+  {
+    wifi.isEnabled((isEnabled) => {
+    if (isEnabled)
+      {
+        wifi.connectionStatus((isConnected) => {
+          if (isConnected) {
+              wifi.getSSID((ssid) => {
+                this.setState({msg: "Connected: "+ssid});
+              });
+            } else {
+              this.setState({msg: "Not connected!"});
+          }
+        });
+
+        this.setState({msg: "Wifi is enabled!"});
+      }
+      else
+      {
+        this.setState({msg: "Wifi not enabled!"});
+      }
+    });
+  }
+
+  componentDidMount()
+  {
+    this.UpdateWifiState();
+    this.interval = setInterval(()=> this.UpdateWifiState(), 1000)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{this.state.msg}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
