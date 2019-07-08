9cae4f8dc0bc2336d59796d0472e2fa98616fff9
Rakibul Hasan <rakib_cse_062@yahoo.com>
2019-07-07 18:52:42 -0700

    Save permission response

--- UI/servicePermission.js
+++ UI/servicePermission.js
@@ -12,16 +12,24 @@ const serviceFileLocal = RNFS.DocumentDirectoryPath+'/services.js';
 import commonStyle from './Style'
 
 
- var radioOptions = [
-    {label: 'Yes, I will allow access to any relevant parts of the conversation', value: 0 },
-    {label: 'I will only allow access if I could censor certain parts of the relevant conversation', value: 1 },
-    {label: 'No, I will not allow access to any relevant parts of the conversation', value:2}
-  ];
+const fullShare= 0;
+const partialShare= 1;
+const noShare = 2;
 
+ var radioOptions = [
+    {label: 'Yes, I will allow access to any relevant parts of the conversation', value: fullShare },
+    {label: 'I will only allow access if I could censor certain parts of the relevant conversation', value: partialShare },
+    {label: 'No, I will not allow access to any relevant parts of the conversation', value: noShare}
+  ];
 
 export default class ServicePermissionScreen extends React.Component {
 
-  state={serviceName:"NO-SERVICE", selectedValue:0, notAtAllSelected:false, partsSelected:false}
+  state= {  serviceName:"NO-SERVICE",
+            sharingDecision:fullShare,
+            whyNoShare: '',
+            whyPartShare: '',
+            partsToRedact:''
+         }
 
 
   constructor(props) 
@@ -36,20 +44,16 @@ export default class ServicePermissionScreen extends React.Component {
     this.setState({serviceName: serviceName});
   }
 
-  selectionChanged(value)
+
+  saveResponse()
   {
-    if(value==0)
-    {
-        this.setState({partsSelected:false, notAtAllSelected:false})
-    }
-    if(value==1)
-    {
-        this.setState({partsSelected:true, notAtAllSelected:false})
-    }
-    if(value==2)
-    {
-        this.setState({notAtAllSelected:true, partsSelected:false})
-    }
+       _permissionResponse= {
+          "Sharing": this.state.sharingDecision,
+          "PartsToRedact": this.state.partsToRedact,
+          "WhyPartShare": this.state.whyPartShare,
+          "WhyNoShare": this.state.whyNoShare,
+       }
+       Alert.alert(JSON.stringify(_permissionResponse));
   }
 
   render() {
@@ -73,37 +77,40 @@ export default class ServicePermissionScreen extends React.Component {
           <RadioForm style={commonStyle.radioFrameStyle}
               radio_props={radioOptions}
               initial={0}
-              onPress={(value) => this.selectionChanged(value)}
+              onPress={(value) => {
+                    this.setState({sharingDecision:value});
+                }
+              }
           />
 
-          { this.state.partsSelected &&
+          { (this.state.sharingDecision == partialShare) &&
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text style={commonStyle.questionStyle}>
                     What parts would you not allow the device to access?
                 </Text>
                 <TextInput multiline={true} numberOfLines={4} style={commonStyle.inputStyle}
-                    onChangeText={(text) => this.setState({ conversationTopic: text })}
-                    value={this.state.conversationTopic}
+                    onChangeText={(text) => this.setState({ partsToRedact: text })}
+                    value={this.state.partsToRedact}
                 />
 
                 <Text style={commonStyle.questionStyle}>
                     Why would you not allow the device to access these parts?
                 </Text>
                  <TextInput multiline={true} numberOfLines={4} style={commonStyle.inputStyle}
-                    onChangeText={(text) => this.setState({ conversationTopic: text })}
-                    value={this.state.conversationTopic}
+                    onChangeText={(text) => this.setState({ whyPartShare: text })}
+                    value={this.state.whyPartShare}
                 />
             </View>
           }
 
-          { this.state.notAtAllSelected &&
+          { (this.state.sharingDecision == noShare) &&
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text style={commonStyle.questionStyle}>
                     Why would you not allow to access the relevant conversation?
                 </Text>
                 <TextInput multiline={true} numberOfLines={4} style={commonStyle.inputStyle}
-                    onChangeText={(text) => this.setState({ conversationTopic: text })}
-                    value={this.state.conversationTopic}
+                    onChangeText={(text) => this.setState({ whyNoShare: text })}
+                    value={this.state.whyNoShare}
                 />
             </View>
           }
@@ -112,7 +119,11 @@ export default class ServicePermissionScreen extends React.Component {
       <View style={commonStyle.buttonViewStyle}>
           <TouchableHighlight style ={commonStyle.buttonTouchHLStyle}>
             <Button
-              onPress={() => this.props.navigation.navigate('ContextualQuestion')}
+              onPress={() => {
+                    this.saveResponse();
+                    this.props.navigation.navigate('ContextualQuestion');
+                }
+              }
               title="Next"
               color="#20B2AA"
               accessibilityLabel="Next"
