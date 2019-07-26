8f98d998b158be1d9f5952d4c0118a9dc6c2c05d
Rakibul Hasan <rakib_cse_062@yahoo.com>
2019-07-27 00:54:02 +0200

    Function to check if file exists

--- controllers/utilities.js
+++ controllers/utilities.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button, Alert} from 'react-native';
+import {Platform, StyleSheet, Text, View, AsyncStorage, Alert} from 'react-native';
 import * as RNFS from 'react-native-fs';
 import wifi from 'react-native-android-wifi';
 import logger from './logger';
@@ -38,81 +38,120 @@ class Utilities extends Component
        return _ssid;
    }
 
+   async fileExists(path, fileName, callerClass, callerFunc)
+   {
+        logger.info(`${callerClass}`, `${callerFunc}-->fileExists`, 'Checking if file exists:'+fileName);
+        if (await RNFS.exists(path))
+        {
+            return true;
+        }
+        return false;
+
+   }
 
     async writeJSONFile(content, fileName, callerClass, callerFunc)
     {
-        //if there is an existing file, create a backup first
+        //NOTE: send JSON object to write, DO NOT stringify
 
+        //if there is an existing file, create a backup first
          if (await RNFS.exists(fileName))
          {
              logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'File already exists.');
 
              RNFS.copyFile(fileName, fileName+'.backup')
-                 .then( (success) => {
-                     logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Backed up existing file.');
+                 .then( (success) => // 1S
+                 {
+                     logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Created backup copy of the file.');
 
                      RNFS.writeFile(fileName, JSON.stringify(content))
-                         .then( (success) => {
-                             logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Created new file.');
-                               RNFS.unlink(fileName+'.backup')
-                                   .then(() => {
-                                     logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Deleted old file.');
-                                   })
-                                   // `unlink` will throw an error, if the item to unlink does not exist
-                                   .catch((err) => {
-                                      logger.error(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Failed to delete backup file:'+error.message);
-                                   });
+                         .then( (success) => // 2S
+                         {
+                             logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Wrote new content in the original file.');
+
+                             RNFS.writeFile(fileName, JSON.stringify(content))
+                                 .then( (success) => // 2S
+                                 {
+                                       logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Created new file.');
+                                       RNFS.unlink(fileName+'.backup')
+                                           .then(() => // 3S
+                                           {
+                                             logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Deleted backup file.');
+                                           })
+                                           // `unlink` will throw an error, if the item to unlink does not exist
+                                           .catch((error) => //3E
+                                           {
+                                              logger.error(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Failed to delete backup file:'+error.message);
+                                           });
+                                 })
+                                 .catch( (error)=> // 2E
+                                 {
+                                     logger.error(`${callerClass}`, `${callerFunc}-->writeJSONFile`,'Error in creating new file:'+error.message+'. Restoring backup.');
+
+                                     RNFS.copyFile(fileName+'.backup', fileName)
+                                         .then( (success)=> //4S
+                                         {
+                                             logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Restored backup file.');
+                                         })
+                                         .catch( (error)=> //4E
+                                         {
+                                             logger.error(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Failed to Restore backup file:'+error.message);
+                                         })
+
+
+                                 })
+
                          })
-                         .catch( (error)=>{
+                         .catch( (error)=> // 2E
+                         {
                              logger.error(`${callerClass}`, `${callerFunc}-->writeJSONFile`,'Error in creating new file:'+error.message+'. Restoring backup.');
-
-                             RNFS.copyFile(fileName+'.backup', fileName)
-                                 .then( (success)=> {
-                                     logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Restored backup file.');
-                                 })
-                                 .catch( (error)=>{
-                                     logger.error(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Failed to Restore backup file:'+error.message);
-                                 })
-
                          })
                  })
-                 .catch( (error) => {
-                     logger.error(`${codeFileName}`, `${callerFunc}-->writeJSONFile`, 'Error backing up file:'+error.message);
+                 .catch( (error) => // 1E
+                 {
+                     logger.error(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Error backing up file:'+error.message);
                  })
          }
          else
          {
             RNFS.writeFile(fileName, JSON.stringify(content))
-                 .then( (success) => {
+                 .then( (success) =>
+                 {
                      logger.info(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Created new file: '+fileName);
                  })
-                 .catch( (error)=>{
+                 .catch( (error)=>
+                 {
                       logger.error(`${callerClass}`, `${callerFunc}-->writeJSONFile`, 'Failed to create file:'+error.message);
-                  })
+                 })
          }
     }
 
-    async readJSONFile(filePath, callerClass, callerFunc)
+    readJSONFile(filePath, callerClass, callerFunc)
     {
 
-         if (await RNFS.exists(filePath))
-         {
-            logger.info(callerClass, callerFunc+"-->readJSONFile", 'Reading file:'+filePath);
-             RNFS.readFile(filePath)
-                 .then((_fileContent) => {
-                    logger.info(callerClass, callerFunc+"-->readJSONFile", 'Successfully read file.');
-                     return _fileContent;
-                 })
-                 .catch( (error)=>{
-                       logger.error(callerClass, callerFunc+"-->readJSONFile", 'Failed to read file:'+error.message);
-                   })
+        RNFS.exists(filePath)
+            .then( (exists) =>
+            {
+                if (exists)
+                {
+                     logger.info(callerClass, callerFunc+"-->readJSONFile", 'Reading file:'+filePath);
+                     RNFS.readFile(filePath)
+                         .then((_fileContent) =>
+                         {
+                             logger.info(callerClass, callerFunc+"-->readJSONFile", 'Successfully read file:'+_fileContent);
+                             return _fileContent;
+                         })
+                         .catch( (error)=>
+                         {
+                               logger.error(callerClass, callerFunc+"-->readJSONFile", 'Failed to read file:'+error.message);
+                         })
+                }
+                else
+                {
+                     logger.error(callerClass, callerFunc+"-->readJSONFile", 'Reading file:'+filePath+' does not exist');
+                }
+            });
 
-         }
-         else
-         {
-            logger.error(callerClass, callerFunc+"-->readJSONFile", 'Reading file:'+filePath+' does not exist');
-         }
-
+            logger.info(callerClass, callerFunc+"-->readJSONFile", 'Returning null');
          return null;
     }
 
@@ -123,7 +162,7 @@ class Utilities extends Component
         RNFS.readFile(this.serviceFileLocal)
             .then((_fileContent) => {
 
-              logger.info(`${codeFileName}`,"ReadServiceFile", 'Successfully read:'+this.serviceFileLocal);
+              logger.info(`${callerClass}`,"ReadServiceFile", 'Successfully read:'+this.serviceFileLocal);
 
               _serviceCategoriesJS = JSON.parse(_fileContent).serviceCategories;
               for(var i=0; i< _serviceCategoriesJS.length; i++)
@@ -151,12 +190,12 @@ class Utilities extends Component
                 );
               }
 
-              logger.info(`${codeFileName}`,"ReadServiceFile", 'Number of categories found:'+_serviceCategories.length);
+              logger.info(`${callerClass}`,"ReadServiceFile", 'Number of categories found:'+_serviceCategories.length);
 
               return _serviceCategories;
             })
             .catch((err) => {
-              logger.info(`${codeFileName}`,"ReadServiceFile", 'Failed to read:'+this.serviceFileLocal+". Err:"+err.message);
+              logger.info(`${callerClass}`,"ReadServiceFile", 'Failed to read:'+this.serviceFileLocal+". Err:"+err.message);
               return _serviceCategories;
             })
 
