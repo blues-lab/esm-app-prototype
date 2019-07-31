b4f43afacb977a6b614bfc167228595d60b30572
Rakibul Hasan <rakib_cse_062@yahoo.com>
2019-07-31 10:42:47 -0700

    MINOR: more logging

--- UI/toolbar.js
+++ UI/toolbar.js
@@ -19,12 +19,17 @@ class ToolBar extends React.Component {
   //var { navigate } = this.props.navigation;
   componentDidMount()
   {
+    logger.info(codeFileName, 'componentDidMount', 'getting appStatus');
     const _appStatus = appStatus.getStatus();
     const _lastNotificationTime = _appStatus.LastNotificationTime;
     const _curTime = new Date();
 
+    logger.info(codeFileName, 'componentDidMount', 'curTime:'+_curTime.toString()+'. _lastNotificationTime:'+_lastNotificationTime.toString());
     const _secondsPassed = (_curTime.getTime() - _lastNotificationTime.getTime())/1000;
     const _secRemaining = _appStatus.PromptDuration * 60 - _secondsPassed;
+
+    logger.info(codeFileName, 'componentDidMount', '_secRemaining:'+_secRemaining);
+
 
     this.setState({minRemaining: Math.floor(_secRemaining/60), secRemaining: Math.floor(_secRemaining%60)})
     this.interval = setInterval(()=> this.updateTimeDisplay(), 1000)
