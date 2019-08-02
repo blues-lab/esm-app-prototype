e3a11650b88f1f835484c021f68ef30cb637d201
Nathan Malkin <nmalkin@cs.berkeley.edu>
2019-08-01 17:52:43 -0700

    Automatically format source files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     es6: true
   },
-  extends: ["standard", "prettier"],
+  extends: ["standard", "plugin:prettier/recommended"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
