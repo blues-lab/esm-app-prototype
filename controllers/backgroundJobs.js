dfef6d791b34b9412de3f7075b271924a030a254
Rakibul Hasan <rakib_cse_062@yahoo.com>
2019-08-06 18:58:59 -0700

    MINOR: use cent symbol

--- controllers/backgroundJobs.js
+++ controllers/backgroundJobs.js
@@ -213,7 +213,7 @@ export async function showPrompt()
                       _remainingTime = PROMPT_DURATION;
                       notificationController.cancelNotifications();
                       notificationController.showNotification("New survey available!",
-                        "Complete within "+_remainingTime+" minutes to get \u002420!!!");
+                        "Complete within "+_remainingTime+" minutes to get \u00A220!!!");
 
                       const _currentDate = new Date();
                       await appStatus.setLastSurveyCreationDate(_currentDate)
@@ -256,7 +256,7 @@ export async function showPrompt()
 
                   notificationController.cancelNotifications();
                   notificationController.showNotification("New survey available!",
-                                                          "Complete within "+_remainingTime+" minutes to get \u002420!!!");
+                                                          "Complete within "+_remainingTime+" minutes to get \u00A220!!!");
                   logger.info(codeFileName,"showPrompt", "Showing latest notification at: "+new Date());
                   await appStatus.setLastNotificationTime(new Date());
 
