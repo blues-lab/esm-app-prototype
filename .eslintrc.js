a0e2489edc238859c91da53d4bdc48dafdeaf142
Nathan Malkin <nmalkin@cs.berkeley.edu>
2019-09-04 23:28:02 -0700

    Switch eslint config from standard to airbnb
    because the latter has React-specific rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     es6: true
   },
-  extends: ["standard", "prettier"],
+  extends: ["eslint:recommended", "airbnb", "prettier"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
@@ -19,7 +19,10 @@ module.exports = {
     "spaced-comment": 1,
     "no-unused-vars": 1,
     "lines-between-class-members": 1,
-    "import/first": 1
+    "import/first": 1,
+    "spaced-comment": 0,
+    "prefer-template": 1,
+    "react/jsx-filename-extension": 0
   },
   parser: "babel-eslint"
 };
