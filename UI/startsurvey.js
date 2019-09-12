4c3393653525570bc0efdd7464e447fc0bb28ff4
Rakibul Hasan <rakib_cse_062@yahoo.com>
2019-09-12 13:51:44 -0400

    Fix: keyboard was not dismissed for iOS

--- UI/startsurvey.js
+++ UI/startsurvey.js
@@ -10,7 +10,9 @@ import {
   BackHandler,
   TouchableHighlight,
   Dimensions,
-  Modal
+  Modal,
+  TouchableWithoutFeedback,
+  Keyboard
 } from "react-native";
 import { ProgressDialog } from "react-native-simple-dialogs";
 import logger from "../controllers/logger";
@@ -122,116 +124,119 @@ export default class SurveyStartScreen extends React.Component {
           margin: 5
         }}
       >
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          <Text style={commonStyles.questionStyle}>{TALKING_ABOUT}</Text>
-          <TextInput
-            multiline={true}
-            numberOfLines={4}
-            style={commonStyles.inputStyle}
-            onChangeText={text => this.setState({ conversationTopic: text })}
-            value={this.state.conversationTopic}
-          />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <Text style={commonStyles.questionStyle}>{TALKING_ABOUT}</Text>
 
-          <Text style={{ fontSize: 14, fontStyle: "italic", margin: 10 }}>
-            {TALKING_ABOUT_SKIP_HINT}
-          </Text>
+            <TextInput
+              multiline={true}
+              numberOfLines={4}
+              style={commonStyles.inputStyle}
+              onChangeText={text => this.setState({ conversationTopic: text })}
+              value={this.state.conversationTopic}
+            />
 
-          <View style={commonStyles.buttonViewStyle}>
-            <TouchableHighlight style={commonStyles.buttonTouchHLStyle}>
-              <Button
-                title="Next"
-                color="#20B2AA"
-                onPress={async () => {
-                  if (this.state.conversationTopic.length == 0) {
-                    Alert.alert(
-                      "Error",
-                      "Please enter conversation topic to continue."
-                    );
-                  } else {
-                    //upload partial survey response
-                    {
-                      this.setState({ saveWaitVisible: true });
-                      const _appStatus = await appStatus.loadStatus();
-                      logger.info(
-                        codeFileName,
-                        "NextButtonPress",
-                        "Uploading partial response and navigating to AlvaPrompt."
-                      );
-                      utilities.uploadData(
-                        {
-                          SurveyID: _appStatus.CurrentSurveyID,
-                          Stage: "Conversation topic.",
-                          PartialResponse: this.state.conversationTopic
-                        },
-                        _appStatus.UUID,
-                        "PartialSurveyResponse",
-                        codeFileName,
-                        "NextButtonPress",
-                        this.fileUploadCallBack.bind(this)
-                      );
+            <Text style={{ fontSize: 14, fontStyle: "italic", margin: 10 }}>
+              {TALKING_ABOUT_SKIP_HINT}
+            </Text>
 
-                      this.setState({ saveWaitVisible: false });
-                    }
-                    this.props.navigation.navigate("AlvaPrompt", {
-                      conversationTopic: this.state.conversationTopic,
-                      surveyProgress: 20
-                    });
-                  }
-                }}
-              />
-            </TouchableHighlight>
-          </View>
-
-          <Modal visible={this.state.noSurveyDialogVisible}>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: "column",
-                justifyContent: "space-between",
-                alignItems: "center",
-                backgroundColor: "lavender",
-                margin: 1
-              }}
-            >
-              <Text
-                style={{
-                  margin: 15,
-                  fontSize: 20,
-                  borderBottomColor: "black",
-                  borderBottomWidth: StyleSheet.hairlineWidth,
-                  padding: 5
-                }}
-              >
-                Sorry, survey has expired!
-              </Text>
-              <Text style={{ fontSize: 16, margin: 20, marginTop: 10 }}>
-                Advertisement/reminder for MIMI and suggestion to come back
-                later.
-              </Text>
-              <TouchableHighlight style={[commonStyles.buttonTouchHLStyle]}>
+            <View style={commonStyles.buttonViewStyle}>
+              <TouchableHighlight style={commonStyles.buttonTouchHLStyle}>
                 <Button
-                  title="I will come back later!"
+                  title="Next"
                   color="#20B2AA"
-                  onPress={() => {
-                    logger.info(
-                      `${codeFileName}`,
-                      "No survey modal",
-                      "Closing app"
-                    );
-                    this.setState({ noSurveyDialogVisible: false });
+                  onPress={async () => {
+                    if (this.state.conversationTopic.length < 0) {
+                      Alert.alert(
+                        "Error",
+                        "Please enter conversation topic to continue."
+                      );
+                    } else {
+                      //upload partial survey response
+                      {
+                        this.setState({ saveWaitVisible: true });
+                        const _appStatus = await appStatus.loadStatus();
+                        logger.info(
+                          codeFileName,
+                          "NextButtonPress",
+                          "Uploading partial response and navigating to AlvaPrompt."
+                        );
+                        utilities.uploadData(
+                          {
+                            SurveyID: _appStatus.CurrentSurveyID,
+                            Stage: "Conversation topic.",
+                            PartialResponse: this.state.conversationTopic
+                          },
+                          _appStatus.UUID,
+                          "PartialSurveyResponse",
+                          codeFileName,
+                          "NextButtonPress",
+                          this.fileUploadCallBack.bind(this)
+                        );
+
+                        this.setState({ saveWaitVisible: false });
+                      }
+                      this.props.navigation.navigate("AlvaPrompt", {
+                        conversationTopic: this.state.conversationTopic,
+                        surveyProgress: 20
+                      });
+                    }
                   }}
                 />
               </TouchableHighlight>
             </View>
-          </Modal>
-        </View>
+
+            <Modal visible={this.state.noSurveyDialogVisible}>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  backgroundColor: "lavender",
+                  margin: 1
+                }}
+              >
+                <Text
+                  style={{
+                    margin: 15,
+                    fontSize: 20,
+                    borderBottomColor: "black",
+                    borderBottomWidth: StyleSheet.hairlineWidth,
+                    padding: 5
+                  }}
+                >
+                  Sorry, survey has expired!
+                </Text>
+                <Text style={{ fontSize: 16, margin: 20, marginTop: 10 }}>
+                  Advertisement/reminder for MIMI and suggestion to come back
+                  later.
+                </Text>
+                <TouchableHighlight style={[commonStyles.buttonTouchHLStyle]}>
+                  <Button
+                    title="I will come back later!"
+                    color="#20B2AA"
+                    onPress={() => {
+                      logger.info(
+                        `${codeFileName}`,
+                        "No survey modal",
+                        "Closing app"
+                      );
+                      this.setState({ noSurveyDialogVisible: false });
+                    }}
+                  />
+                </TouchableHighlight>
+              </View>
+            </Modal>
+          </View>
+        </TouchableWithoutFeedback>
         <ProgressDialog
           visible={this.state.saveWaitVisible}
           title="MiMi"
