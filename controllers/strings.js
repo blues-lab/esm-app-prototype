f38da301bb4303b8031c456eb32ee05923119daa
Nathan Malkin <nmalkin@cs.berkeley.edu>
2019-10-14 13:08:07 -0700

    Edit imagine prompt

--- controllers/strings.js
+++ controllers/strings.js
@@ -87,7 +87,7 @@ export const TALKING_ABOUT_REQUIRED =
 /* Questions about relevant services*/
 export const PROMPT_SCREEN_TITLE = "ALVA"; // TODO: I don't think this is actually seen anywhere?
 export const IMAGINE_TEXT = format(
-  "Imagine that ALVA, the **always-listening voice assistant**, was able to offer you services that were **relevant to the conversation you just had.**"
+  "Imagine that ALVA, the always-on voice assistant, was able to offer you services that were **relevant to the conversation you just had.**"
 );
 export const SELECT_RELEVANT_SERVICES = format(
   "What services could ALVA offer **based on the conversation you just had**? (They don't have to be services you would want or use.)"
