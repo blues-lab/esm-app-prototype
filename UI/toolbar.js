405be8087e0d42eb8466834bde913b2dec33b527
Rakibul Hasan <rakib_cse_062@yahoo.com>
2019-11-08 17:51:51 -0500

    Fix #105

--- UI/toolbar.js
+++ UI/toolbar.js
@@ -349,7 +349,8 @@ class ToolBar extends React.Component {
               alignItems: "center"
             }}
           >
-            {this.state.minRemaining <= 15 && this.state.minRemaining >= 0 && (
+            {this.state.minRemaining!==null && this.state.secRemaining!==null &&
+             this.state.minRemaining <= 15 && this.state.minRemaining >= 0 && (
               <Text style={{ fontSize: 20 }}>
                 {this.state.minRemaining > 9
                   ? this.state.minRemaining
